b46a90874c1b810eb3315d5ae4397fa6bb3adfc9
JOHS\zlwlw <johs970917@gmail.com>
2024-05-15 22:03:53 +0900

    vue-cli 실습

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,33 +1,25 @@
 <template>
-  <AppHeader 
-    v-bind:appTitle="message"
-    v-on:change="changeMessage">
-  </AppHeader>
+  <AppHeader v-bind:appTitle="message"></AppHeader>
 </template>
 
 <script>
-// import 컴포넌트이름 from './컴포넌트 경로'
 import AppHeader from './components/AppHeader.vue'
 
-export default {
-  components: {
-    // '컴포넌트이름': 컴포넌트 내용
-    // 'app-header': AppHeader,
-    // 'AppHeader': AppHeader,
-    // AppHeader: AppHeader,
-    AppHeader
-  },
-  data() {
-    return {
-      message: '앱 헤더 컴포넌트'
-    }
-  },
-  methods: {
-    changeMessage() {
-      this.message = '변경됨'
+  export default {
+    components:{
+      AppHeader
+    },
+    data() {
+      return {
+        message:'앱 헤더 컴포넌트'
+      }
+    },
+    methods:{
+      showAlert(){
+        alert('hello')
+      }
     }
   }
-}
 </script>
 
 <style scoped>
--- vue3-cli/src/components/AppHeader.vue
+++ vue3-cli/src/components/AppHeader.vue
@@ -1,17 +1,11 @@
 <template>
-  <h1>{{ appTitle }}</h1>
-  <button @click="changeTitle">변경</button>
+    <h1>{{appTitle}}</h1>
 </template>
 
 <script>
-  export default {
-    props: ['appTitle'],
-    methods: {
-      changeTitle() {
-        this.$emit('change');
-      }
+    export default {
+        props: ['appTitle']
     }
-  }
 </script>
 
 <style scoped>
