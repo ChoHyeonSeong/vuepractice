a7d9f5f717360fc377eda562f30eb8a418e686b7
JOHS\zlwlw <johs970917@gmail.com>
2024-06-01 23:59:34 +0900

    컴포넌트 패턴

---
+++ controlled/src/components/AppBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input type="checkbox" :value="appValue" @click="toggleCheck" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["appValue"],
+  methods: {
+    toggleCheck() {
+      this.$emit("input",!this.value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ slots/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <ul>
+        <app-item v-for="(item, index) in appItems" :key="index">
+          {{ item }}
+        </app-item>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppItem from "./components/AppItem.vue";
+
+export default {
+  components: {
+    AppItem,
+  },
+  data() {
+    return {
+      appItems: ["1", "2", "3", "4", "5"],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
